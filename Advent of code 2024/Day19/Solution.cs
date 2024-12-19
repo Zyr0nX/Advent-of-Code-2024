664b5c35baf576725081a724c81bfed9873756f4
Duy Dang <d.dang@eml.com.au>
2024-12-19 12:33:48 +0700

    Day 19 part 1

---
+++ Advent of code 2024/Day19/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace Advent_of_code_2024;
+
+public class SolutionDay19() : SolutionBase(19)
+{
+    public override string Part1Solver()
+    {
+        var inputSpan = Input.AsSpan();
+        var separator = inputSpan.IndexOf($"{Environment.NewLine}{Environment.NewLine}");
+        var towelsSpan = inputSpan[..separator];
+        var designsSpan = inputSpan[(separator + Environment.NewLine.Length * 2)..];
+
+        var towels = new HashSet<string>();
+        foreach (var towelRange in towelsSpan.Split(", "))
+        {
+            var towel = towelsSpan[towelRange];
+            towels.Add(towel.ToString());
+        }
+
+        var res = 0;
+        foreach (var design in designsSpan.EnumerateLines())
+        {
+            var stack = new Stack<int>();
+            stack.Push(0);
+            while (stack.TryPop(out var idx))
+            {
+                for (int i = idx; i < design.Length; i++)
+                {
+                    var part = design[idx..i];
+
+                    if (towels.Contains(part.ToString()))
+                    {   
+                        if (i == design.Length - 1)
+                        {
+                            res++;
+                            stack.Clear();
+                            break;
+                        }
+                        stack.Push(i);
+                    }
+                }
+            }
+        }
+
+        return res.ToString();
+    }
+
+    public override string Part2Solver()
+    {
+        throw new NotImplementedException();
+    }
+}
